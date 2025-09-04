b39db6c1db050b53b1e9ececb8950ea5238b224a
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-09-04 15:49:38 -0300

    BBOSPP 1: quase pronto

--- bigBookOfSmallPythonProjects/p1_bagels.py
+++ bigBookOfSmallPythonProjects/p1_bagels.py
@@ -2,34 +2,61 @@ import random
 
 def main():
     prints()
+    fermiCount = 0
+    picoCount = 0
 
     r = random.randint(100, 999)
+    print(r)
 
     rArray = digits(r)
-    for i in range(3):
-        print(rArray[i])
+    """for i in range(3):
+        print(rArray[i])"""
 
     print()
     for i in range(10):
         print(f"Guess #{i+1}: ")
-        g = input()
+        g = int(input())
 
-        pass
+        gArray = digits(g)
+
+        for i in range(3):
+            if rArray[i] == gArray[i]:
+                fermiCount += 1
+
+        if fermiCount == 0:
+            rArray.sort()
+            gArray.sort()
+
+            for i in range(3):
+                if rArray[i] == gArray[i]:
+                    picoCount += 1
+
+        if fermiCount == 0 and picoCount == 0:
+            print("Bagels")
+        if fermiCount > 0 and picoCount == 0:
+            print("Pico")
+        if fermiCount == 0 and picoCount > 0:
+            print("Fermi")
+        
+        fermiCount = 0
+        picoCount = 0
+
+    print(f"Right number: {r}")
 
 def prints():
     print("I'm thinking of a 3-digit number. Try to guess what it is.")
     print("Here are some clues:")
-    print("When I say :     That means")
-    print("Pico             One digit is corret but in the wrong position.")
-    print("Fermi            One digit is correct but int the right position.")
-    print("Bagels           No digit is correct.")
+    print("When I say:     That means: ")
+    print("Pico            One digit is corret but in the wrong position.")
+    print("Fermi           One digit is correct but in the right position.")
+    print("Bagels          No digit is correct.")
     print("I've thought up a number!")
     print("You have 10 guesses to get it.")
 
 def digits(z):
     xs = [0] * 3
 
-    print(z)
+    #print(z)
 
     u = z%10
     xs[0] = u
