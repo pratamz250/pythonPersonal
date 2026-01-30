d252864c52fb0529b1e4a39dfc4e7c986ce92efa
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-01-30 16:02:19 -0300

    Teste de Composição de Fermat e número giganórmico de 617 dígitos

---
+++ avulsos/fermatTestCompositeness.py
@@ -0,0 +1,26 @@
+#Not naive at all (because we are in Python) programm to make the Fermat Test of Compositeness
+
+import math
+
+def main():
+    n = int(input().strip())
+    cong = set()
+    count = 0
+
+    for a in range(1, n):
+        z = pow(a, n-1) % n
+        #print(f"a:{a} congruence:{z}")
+        cong.add(z)
+        if z != 1:
+            count += 1
+
+    if(count != 1):
+        print(f"Composto! {count}\n")
+    else:
+        print(f"Primo! {count}\n")
+
+    print(cong)
+    print(f"Congruencias: {len(cong)}")
+
+if __name__ == "__main__":
+    main()
