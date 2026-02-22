689504490fd0767c48264472e9dc433e4b475e20
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-02-21 21:47:43 -0300

    Continuação

--- webscraping/s2Project/s2.py
+++ webscraping/s2Project/s2.py
@@ -2,24 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 
 def main():
-    url = "http://books.toscrape.com/"
-
-    response = requests.get(url)
-
-    html = response.text
-
-    soup = BeautifulSoup(html, 'html.parser')
-
-    all_h3 = soup.find_all("h3")
-
-    with open("dados2_s2.txt", "w") as file1:
-        for h3 in all_h3:
-            all_a = h3.find("a")
-            file1.write(f"Title: {all_a["title"]}\n")
-
-    print(f"h3: {len(all_h3)}")
-
-    file1.close()
+    for i in range(1, 52):
+        response = request.get("http://books.toscrape.com/catalogue/page-1.html")
 
 if __name__ == "__main__":
     main()
