ab90cc7d9685f2afd5ba4eb5a599562f128209b9
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-02-24 16:58:38 -0300

    Projeto 2 concluído

--- webscraping/s2Project/s2.py
+++ webscraping/s2Project/s2.py
@@ -2,9 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 
 def main():
-    for i in range(1, 52):
-        response = request.get("http://books.toscrape.com/catalogue/page-1.html")
+    for i in range(1, 51):
+        url = f'http://books.toscrape.com/catalogue/page-{i}.html'
 
-if __name__ == "__main__":
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f'Resquest {i} succefull. Link: {url}')
+        else:
+            print(f'Error on request {i}')
+            exit()
+
+        html = response.text
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        all_h3 = soup.find_all('h3')
+
+        with open('dados3_s2.txt', 'a+') as file1:
+            file1.write(f'Books on page {i}:\n')
+            for h3 in all_h3:
+                all_a = h3.find('a')
+                file1.writelines(f'{all_a['title']}\n')
+
+            file1.write(f'Total: {len(all_h3)}\n\n\n')
+
+    file1.close()
+
+if __name__ == '__main__':
     main()
-    print()
+    print('Done')
